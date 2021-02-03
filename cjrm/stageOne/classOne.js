5fa149e2e87c4a5e110f5ce106b976663b3cad3b
andrew herculano <andrewherculano@hotmail.com>
2021-02-03 07:31:24 -0300

    add class one

---
+++ cjrm/stageOne/classOne.js
@@ -0,0 +1,17 @@
+console.log('const, let, var')
+
+//const não pode ser reatribuido um novo valor
+const points = 100000
+console.log(points)
+
+//let pode ser reatribuido um novo valor
+let numberRandom = 44
+console.log(numberRandom)
+numberRandom = 35
+console.log(numberRandom)
+
+//assim como let pode ser reatribuido novo valor
+var names = 'Andrew Herculano'
+console.log(names)
+names = 'newName'
+console.log(names)
