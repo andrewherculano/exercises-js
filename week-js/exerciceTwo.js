32a93b761fb34bd15c6010f1cd6cdfe94cced8e7
andrew herculano <andrewherculano@hotmail.com>
2021-02-02 21:57:36 -0300

    add cjrm

--- week-js/exerciceTwo.js
+++ week-js/exerciceTwo.js
@@ -2,8 +2,8 @@
 
 const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
 
-const numbersLessThen500 = crazyNumbers.filter(function(item){
+const numbersLessThen501 = crazyNumbers.filter(function(item){
     return item < 501
 })
 
-console.log(numbersLessThen500.length)
+console.log(numbersLessThen501.length)
