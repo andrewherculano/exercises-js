a1cf7dd1497e013e2258f87feef1ed0a1b846c6a
andrew herculano <andrewherculano@hotmail.com>
2021-02-04 15:08:33 -0300

    arrays

---
+++ cjrm/stageOne/class03/arrays.js
@@ -0,0 +1,33 @@
+//Arrays
+let heroes = ['Batman', 'Mulher Maravilha', 'Aquamen']
+
+heroes[3] = 'Spider-man'
+console.log(heroes)
+
+ages = [10, 20, 50, 70]
+console.log(ages[3])
+
+const ramdomHeroes = ['Diana', 'Parker', 1, 5]
+console.log(ramdomHeroes)
+
+//Métodos arrays
+const joinHeroes = heroes.join(' | ')
+console.log(joinHeroes)
+
+//Example two
+const indexOf25 = ages.indexOf(50)
+console.log(`A posição é: ${indexOf25}`)
+
+//Example three
+const concatHeroes = heroes.concat('Visão', 'Flash')
+console.log(concatHeroes)
+
+//Example four
+const pushHeroes = heroes.push('Hulk')
+console.log(pushHeroes)
+console.log(heroes)
+
+//Example five
+const popHeroes = heroes.pop('Hulk')
+console.log(popHeroes)
+console.log(heroes)
---
+++ cjrm/stageOne/class03/nullUndefined.js
@@ -0,0 +1,9 @@
+//Example one
+let undefinedOne 
+
+console.log(`O valor da variavel é: ${undefinedOne}`)
+
+//Example two
+
+let nullOne = null
+console.log(`A varivel ${nullOne + 4} é ${nullOne}`)
