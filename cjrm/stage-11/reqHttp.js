b7b0d02f43e8d7c94b6f93c94579983a5985bc45
andrew herculano <andrewherculano@hotmail.com>
2021-05-27 10:14:32 -0300

    requests http, assincronismo

--- cjrm/stage-11/reqHttp.js
+++ cjrm/stage-11/reqHttp.js
@@ -1,10 +1,32 @@
-const request = new XMLHttpRequest()
+const getTodosApi = (url, callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText)
+  request.addEventListener("readystatechange", () => {
+    const isRequestOk = request.readyState === 4 && request.status === 200;
+    const isRequestNotOk = request.readyState === 4;
+
+    if (isRequestOk) {
+      const data = JSON.parse(request.responseText)
+      callback(null, data);
+      return;
     }
-})
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
+    if (isRequestNotOk) {
+      callback("Não foi possivel obter os dados da API", null);
+    }
+  });
+
+  request.open("GET", url);
+  request.send();
+};
+
+getTodosApi('../json/todos-0.json', (error, data) => {
+  console.log(data);
+  getTodosApi('../json/todos-1.json', (error, data) => {
+    console.log(data)
+    getTodosApi('../json/todos-2.json', (error, data) => {
+      console.log(data)
+    })
+  })
+});
+
