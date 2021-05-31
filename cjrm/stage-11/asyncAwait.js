c66201ccd9717bad4be4d8c507852369d34b93ba
andrew herculano <andrewherculano@hotmail.com>
2021-05-31 20:51:15 -0300

    async, await

---
+++ cjrm/stage-11/asyncAwait.js
@@ -0,0 +1,11 @@
+const getUsers = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  return await response.json()
+}
+
+const logUsers = async () => {
+  const users = await getUsers()
+  console.log(users)
+}
+
+logUsers()
