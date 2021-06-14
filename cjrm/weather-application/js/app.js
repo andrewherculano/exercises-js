6279ee8c7622df6538db4f18ad948355236f61ce
andrew herculano <andrewherculano@hotmail.com>
2021-06-14 19:05:24 -0300

    request city weather

--- cjrm/weather-application/js/app.js
+++ cjrm/weather-application/js/app.js
@@ -23,4 +23,26 @@ const getCityData = async (cityName) => {
   }
 }
 
-getCityData('Minas Gerais')
+const getCityWeatherData = async (cityName) => {
+  try {
+    const { Key } = await getCityData(cityName)
+    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`
+    const response = await fetch(cityWeatherUrl)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    const [cityWeatherData] = await response.json()
+    debugger
+    return cityWeatherData
+
+  } catch ({
+    name,
+    message
+  }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+getCityWeatherData('Minas Gerais')
