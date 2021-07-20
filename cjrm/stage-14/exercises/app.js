9c952b81ec0f226aea0d09f965fa4de4c1a6e948
andrew herculano <andrewherculano@hotmail.com>
2021-07-20 18:38:08 -0300

    carrousel button prev

--- cjrm/stage-14/exercises/app.js
+++ cjrm/stage-14/exercises/app.js
@@ -173,16 +173,30 @@ console.log(accumulator)
 */
 
 const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]')
 const slides = document.querySelectorAll('[data-js="carousel__item"]')
 
 let currentSlideIndex = 0
 
 nextButton.addEventListener('click', () => {
-  
   if (currentSlideIndex === slides.length -1) {
     currentSlideIndex = 0
   } else {
     currentSlideIndex++
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove('carousel__item--visible')
+  })
+
+  slides[currentSlideIndex].classList.add('carousel__item--visible')
+})
+
+prevButton.addEventListener('click', () => {
+  if (currentSlideIndex === 0) {
+    currentSlideIndex = slides.length -1
+  } else {
+    currentSlideIndex--
   }
 
   slides.forEach(slide => {
